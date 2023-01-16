f8d910ed067c7fbd45a9c95c5f7fe039cc53015e
rootx-programmer <122776153+rootx-programmer@users.noreply.github.com>
2023-01-16 13:48:23 +0530

    Delete web app/airline reservation system directory

--- web app/airline reservation system/flights.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="shadow-lg p-5 mb-5 bg-white rounded">
-        <div id="flights">
-            <?php include('avflights.php');  ?>
-        </div>
-
-
-    </div>
-</div>
